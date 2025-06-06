a8ba484282c133455d4bc8728766955ce033d1a0
cullenowens <cullenowens2005@gmail.com>
2025-06-06 12:37:26 -0400

    finish making tts object and adjustments

--- src/Back-End/app.py
+++ src/Back-End/app.py
@@ -1,12 +1,37 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import os
 from docx import Document
+import pyttsx3
 
 app = Flask(__name__,
             template_folder=os.path.join(os.pardir, 'templates'),
             static_folder=os.path.join(os.pardir, 'static'))
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+#TTS Class
+class TextToSpeech:
+    def __init__(self, output_dir='frontend/static/audio', filename='output.mp3'):
+        self.engine = pyttsx3.init()
+        self.output_dir = output_dir
+        self.filename = filename
+        os.makedirs(self.output_dir, exist_ok=True)
+    
+    def set_voice(self, gender='male'):
+        voices = self.engine.getProperty('voices')
+        for voice in voices:
+            if gender in voice.name.lower():
+                self.engine.setProperty('voice', voice.id)
+                break
+    
+    def set_rate(self, rate=150):
+        self.engine.setProperty('rate', rate)
+    
+    def generate_audio(self, text):
+        audio_path = os.path.join(self.output_dir, self.filename)
+        self.engine.save_to_file(text, audio_path)
+        self.engine.runAndWait()
+        return audio_path
 
 @app.route('/')
 def home():
@@ -30,7 +55,9 @@ def upload_file():
         content = "\n".join([para.text for para in doc.paragraphs])
     else:
         return jsonify({'error': 'Unsupported file type'}), 400
+
     return jsonify({'content': content})
+
 
 if __name__ == '__main__':
     app.run(debug=True)

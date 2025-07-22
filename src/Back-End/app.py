19a555201385044f32009a70a23b401095c3f34d
cullenowens <cullenowens2005@gmail.com>
2025-07-22 17:23:09 -0400

    not right

--- src/Back-End/app.py
+++ src/Back-End/app.py
@@ -15,8 +15,7 @@ from gtts import gTTS
 
 class TextToSpeech:
     def __init__(self, output_dir=os.path.join(os.pardir, 'static', 'audio'), filename='output.mp3'):
-        #self.output_dir = output_dir
-        self.output_dir = os.path.abspath(os.path.dirname(__file__)),
+        self.output_dir = output_dir
         print(f"[DEBUG] Output directory: {self.output_dir}")
         self.filename = filename
         print(f"[DEBUG] Filename: {self.filename}")
